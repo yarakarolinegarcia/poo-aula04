cf4129c3932135ccf48d5f9113a23334b895e297
Yara Garcia <yarakarolinegarcia@gmail.com>
2018-02-23 14:57:10 -0300

    v2

--- OlaMundo.java
+++ OlaMundo.java
@@ -1,8 +1,8 @@
 public class OlaMundo{
 
 	public static void main(String[] args){
-		System.out.println("Ola mundo!");
-
+		System.out.println("Olá mundo!");
+		System.out.println("Olá, Yara!");
 	}
 }
 
